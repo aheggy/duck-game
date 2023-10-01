4ddac20dc4d0f5da9e86f36be20b4dc4d257a08c
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-10-01 12:05:43 -0400

    duck is now moving

--- game.js
+++ game.js
@@ -4,8 +4,22 @@ const ctx = canvas.getContext("2d");
 let duckImage = new Image();
 duckImage.src = "duck.png";
 
-setTimeout(() => {
-    console.log(ctx)
-    console.log(canvas)
-  ctx.drawImage(duckImage, 0, 0);
-}, 1000);
+class GameObject {
+  constructor(x = 0, y = 0) {
+    this.image = null;
+    this.pos = {
+      x: x,
+      y: y,
+    };
+  }
+}
+
+let duck = new GameObject();
+duck.image = duckImage;
+
+setInterval(() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.drawImage(duck.image, duck.pos.x, duck.pos.y);
+    duck.pos.x += 2;
+    duck.pos.y += 1;
+}, 30);
