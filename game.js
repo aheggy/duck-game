4a52a715711be1449e04fcbbedcf35574ea0833f
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-10-01 13:16:21 -0400

    duck is now jump =

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 const canvas = document.getElementById("canvas");
+canvas.style.imageRendering = "pixelated"
 const ctx = canvas.getContext("2d");
 
 let duckImage = new Image();
@@ -9,17 +10,48 @@ class GameObject {
     this.image = null;
     this.pos = {
       x: x,
-      y: y,
+      y: y
     };
+
+    this.speed = {
+      x: 0,
+      y: 0,
+    };
+  }
+
+  draw() {
+    ctx.drawImage(this.image, this.pos.x, this.pos.y);
+  }
+
+  move() {
+    this.pos.x += this.speed.x;
+    this.pos.y += this.speed.y;
+
+    if (this.pos.y > canvas.height - 34) {
+      this.pos.y = canvas.height - 34;
+    }
+
+    this.speed.y += 1;
+  }
+
+  jump() {
+    if (this.pos.y === canvas.height - 34) {
+      this.speed.y = -20;
+    }
   }
 }
 
-let duck = new GameObject();
+let duck = new GameObject(200, 0);
 duck.image = duckImage;
 
+document.addEventListener("keydown", (event) => {
+  if (event.code === "ArrowUp") {
+    duck.jump();
+  }
+});
+
 setInterval(() => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ctx.drawImage(duck.image, duck.pos.x, duck.pos.y);
-    duck.pos.x += 2;
-    duck.pos.y += 1;
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  duck.move()
+  duck.draw()
 }, 30);
